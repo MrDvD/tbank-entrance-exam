4a9671d473a01cb7c2641889a91ddfa342f0ab4a
MrDvD <ma.labin@yandex.ru>
2025-07-30 20:39:07 +0500

    feat: Add Material Waves effect
    Fix layout to work with new effect

--- src/main/script.js
+++ src/main/script.js
@@ -1,5 +1,7 @@
 import { initEditableTexts } from "../resources/js/editable";
 import { initClearCookies } from "../resources/js/clearCookies";
+import { initRipples } from "../resources/js/ripples";
 
 initEditableTexts();
 initClearCookies();
+initRipples();
---
+++ src/resources/js/ripples.js
@@ -0,0 +1,33 @@
+const rippleElementSelectors = [".card", ".button"];
+
+export function initRipples() {
+  document.addEventListener('DOMContentLoaded', () => {
+    rippleElementSelectors.forEach(selector => {
+      document.querySelectorAll(selector).forEach(elem => {
+        elem.addEventListener('pointerdown', function(event) {
+          if (event.target !== elem) {
+            return;
+          }
+
+          const ripple = document.createElement('div');
+          ripple.className = 'ripple';
+
+          const x = event.offsetX;
+          const y = event.offsetY;
+          ripple.style.left = `${x}px`;
+          ripple.style.top = `${y}px`;
+
+          elem.appendChild(ripple);
+
+          requestAnimationFrame(() => {
+            ripple.classList.add('run');
+          });
+
+          ripple.addEventListener('transitionend', () => {
+            ripple.remove();
+          });
+        });
+      });
+    });
+  });
+}
